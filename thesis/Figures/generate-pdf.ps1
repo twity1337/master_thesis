202549b413b14446ca7027516a269af121d1d07a
Tim Wißmann <twity1337@gmail.com>
2023-02-12 01:05:30 +0100

    Update design doc and graphics

--- thesis/Figures/generate-pdf.ps1
+++ thesis/Figures/generate-pdf.ps1
@@ -1,6 +1,6 @@
 Push-Location $PSScriptRoot
 
 Remove-Item *.pdf
-& "$Env:ProgramFiles\WindowsApps\draw.io.draw.ioDiagrams_20.8.10.0_x64__1zh33159kp73c\app\draw.io.exe" . --crop -x -f pdf .
+& "$Env:ProgramFiles\draw.io\draw.io.exe" . --crop -x -f pdf .
 
 Pop-Location
