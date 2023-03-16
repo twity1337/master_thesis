8d64d3ace11ad93099dae6253eb4b7adcfa2ef07
Tim Wißmann <twity1337@gmail.com>
2023-03-16 04:10:47 +0100

    Add Conclusion, Cluster scenarios and minor fixes

--- thesis/Figures/generate-pdf.ps1
+++ thesis/Figures/generate-pdf.ps1
@@ -1,6 +1,11 @@
+if ([System.Environment]::OSVersion.Version -eq [System.Version]"10.0.19045.0") { # check whether computer 1 or computer 2 is in use.. it's dirty, i know...
+	$exePath = "$Env:ProgramFiles\WindowsApps\draw.io.draw.ioDiagrams_20.8.10.0_x64__1zh33159kp73c\app\draw.io.exe"
+} else {
+	$exePath = "$Env:ProgramFiles\draw.io\draw.io.exe"
+}
+
 Push-Location $PSScriptRoot
 
-Remove-Item *.pdf
-& "$Env:ProgramFiles\draw.io\draw.io.exe" . --crop -x -f pdf .
+& $exePath . --crop -x -a -f pdf .
 
 Pop-Location
